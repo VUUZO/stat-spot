f3dd367d413a79002f6fa65c9a6e1dcafadf1b5b
Oskar <kaplann.oskar@gmail.com>
2023-09-11 17:59:00 +0200

    Added second h2 tag to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ export default async function Home () {
         !song.isPlaying && (
         <>
           <h2 className="mb-8 font-semibold uppercase text-center">[not listening to anything]</h2>
+          <h2 className="mb-8 font-semibold uppercase text-center">[recent fav track]</h2>
 
           <div className="flex gap-8 flex-wrap sm:flex-nowrap items-center p-4">
           <div className={`relative w-full sm:w-20 rounded-3xl aspect-square overflow-hidden`}>
