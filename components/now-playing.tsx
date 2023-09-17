a18539b2dd2f6b9258e0dfc4cb7daad470c6d3db
Oskar <kaplann.oskar@gmail.com>
2023-09-17 14:33:32 +0200

    tailwind quick fix

--- components/now-playing.tsx
+++ components/now-playing.tsx
@@ -28,13 +28,13 @@ const NowPlaying = () => {
                 <h2 className="py-4 uppercase font-bold flex items-center gap-2"><span>Now playing</span> <div className="w-2 aspect-square bg-red-600 rounded-full animate-pulse" /></h2>
 
                 <Link href={playing.songUrl} target="_blank" className="inline-block">
-                  <div className="flex gap-5 items-start">
+                  <div className="flex flex-col sm:flex-row gap-5 items-start">
                     <div className="relative w-[120px] aspect-square shrink-0 rounded-3xl overflow-hidden">
                       <Image alt={'sraka'} src={playing?.albumImageUrl as string} fill className="object-fill absolute"/>
                     </div>
                     <div>
                       <h2 className="font-semibold text-4xl">{playing?.title}</h2>
-                      <p>{playing?.artist}</p>
+                      <p className="text-xl">{playing?.artist}</p>
                     </div>
                   </div>
                 </Link>
