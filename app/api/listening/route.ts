c5da84725d34f17afc5cc0a4c1b58cf57d716742
Oskar <kaplann.oskar@gmail.com>
2023-09-17 18:24:38 +0200

    fix

--- app/api/listening/route.ts
+++ app/api/listening/route.ts
@@ -42,7 +42,8 @@ export async function GET(req: NextRequest) {
         status: 200,
         headers: {
           'content-type': 'application/json',
-          'cache-control': 'public, s-maxage=60, stale-while-revalidate=30'
+          // 'cache-control': 'public, s-maxage=60, stale-while-revalidate=30'
+          'cache-control': 'public, max-age=0'
         }
       }
     );
