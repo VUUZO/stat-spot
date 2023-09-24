bdf4bf3ca2ecb09e8d279fd70881195eaf001dcd
Oskar <kaplann.oskar@gmail.com>
2023-09-24 18:38:01 +0200

    Fixing CSS

--- components/top-tracks.tsx
+++ components/top-tracks.tsx
@@ -18,8 +18,6 @@ const TopTracks = () => {
     error
   } = useSWR<Track[]>('/api/top-tracks', fetcher)
 
-  console.log(tracks)
-  
   return (
     isLoading ? (
       <div></div>
@@ -28,18 +26,18 @@ const TopTracks = () => {
     ) : (
       <>
         <h2 className="py-4 uppercase font-bold">Top tracks [4 weeks]:</h2>
-        <ul className="flex flex-col gap-2">
+        <ul className="grid gap-3 grid-cols-3 lg:grid-cols-[repeat(auto-fit,minmax(350px,1fr))]">
           {
             tracks?.map((track, idx) => (
               
               <li key={track.title}>
-                <div className="flex gap-3 items-start">
-                  <div className="aspect-square w-[50px] relative shrink-0 overflow-hidden rounded-md">
+                <div className="flex flex-col gap-2 sm:gap-4">
+                  <div className="aspect-square w-full relative overflow-hidden rounded-md border-2 border-dark-light">
                     <Image src={track.albumImageUrl} alt={track.title} fill className='object-cover absolute' />
                   </div>
                   <div>
-                    <h3 className="font-semibold">{track.title}</h3>
-                    <p>{track.artist}</p>
+                    <h3 className="font-primary truncate text-xl sm:text-[32px]"><span className="text-green">{idx + 1}.</span> {track.title}</h3>
+                    <p className="font-secondary truncate text-zinc-300 text-xs sm:text-base">{track.artist}</p>
                   </div>
                 </div>
               </li>
