b7367a182d0b1530e3f40e19380ff8a5c2452b7e
Oskar <kaplann.oskar@gmail.com>
2023-09-26 14:15:44 +0200

    Updated favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${lincolnPrimary.variable} ${lincolnSecondary.variable}`}>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="./icons/apple-icon.png" type="image/png" sizes="180x180" />
+        <link rel="apple-touch-icon" sizes="180x180" href="./icons/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="./icons/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="./icons/favicon-16x16.png"/>
+        <link rel="manifest" href="./icons/site.webmanifest"/>
+        <link rel="mask-icon" href="./icons/safari-pinned-tab.svg" color="#91ff5e"/>
+
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
       </head>
       <body className='text-white bg-darkest'>
         {children}
