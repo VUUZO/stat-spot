4f742d977260bff0a57ae299ca57dc3b16ef7d12
Oskar <kaplann.oskar@gmail.com>
2023-09-26 15:21:58 +0200

    Updated favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const lincolnSecondary = localFont({
 })
 
 export const metadata: Metadata = {
-  title: 'trackfm',
+  title: 'Strack',
   description: 'Track your Spotify data',
 }
 
@@ -25,16 +25,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${lincolnPrimary.variable} ${lincolnSecondary.variable}`}>
-      <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#91ff5e"/>
-
-        <meta name="msapplication-TileColor" content="#da532c"/>
-        <meta name="theme-color" content="#ffffff"/>
-      </head>
       <body className='text-white bg-darkest'>
         {children}
       </body>
