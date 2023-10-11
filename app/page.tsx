b398ecc34b7d4a467bd476366e13f7d015c313b1
Oskar <kaplann.oskar@gmail.com>
2023-10-11 15:52:16 +0200

    Updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import NowPlaying from "@/components/now-playing";
 import TopTracks from "@/components/top-tracks";
 
 export default function Home () {
    return (
-    <div className="p-3 sm:p-8">
-      <NowPlaying />
-      <TopTracks />
+    <div className="p-[10px] sm:p-8">
+      <h2>Test Homepage</h2>
     </div>
   )
 }
