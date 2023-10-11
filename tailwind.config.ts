90c2cb7963ad56e7763ae4dfb267271ecac415e4
Oskar <kaplann.oskar@gmail.com>
2023-10-11 15:50:33 +0200

    Implementing grid-areas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,34 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    animation: {
+      'spin-track': 'spin 3s linear infinite',
+    },
+    keyframes: {
+      'spin': {
+        '0%': { transform: 'rotate(0deg)' },
+        '100%': { transform: 'rotate(360deg)' },
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      gridTemplateAreas: {
+        'layout': [
+          'empty play',
+          'navig main',
+        ],
+        'layout-mobile': [
+          'play',
+          'navig',
+          'main',
+        ]
+      },
+      gridTemplateColumns: {
+        'layout': 'auto minmax(0, 1fr)',
+        'layout-mobile': 'minmax(0, 1fr)',
+      },
+      gridTemplateRows: {
+        'layout': 'auto 1fr',
+        'layout-mobile': 'auto auto 1fr',
       },
       fontFamily: {
         primary: ['var(--lincoln-primary)'],
@@ -19,13 +42,15 @@ const config: Config = {
       },
       colors: {
         'darkest': '#131313',
-        'dark': '#161616',
+        'dark': '#181818',
         'dark-light': '#242424',
         'green': '#91FF5E',
-        'green-light': 'rgba(145,255,94,.2)',
-      }
+        'green-light': 'rgba(55,255,99,.2)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@savvywombat/tailwindcss-grid-named-lines')
+  ],
 }
 export default config
