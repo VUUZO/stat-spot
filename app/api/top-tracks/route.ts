35e27e07cd7eaae9a1edba8185c88158851f5ca1
Oskar <kaplann.oskar@gmail.com>
2023-10-11 15:53:05 +0200

    Updated API routes

--- app/api/top-tracks/route.ts
+++ app/api/top-tracks/route.ts
@@ -1,10 +1,13 @@
-import { getTopTracks } from "@/lib/spotify";
+import { getTopTracks, TimeRange } from "@/lib/spotify";
 import { NextResponse, type NextRequest } from "next/server";
 
 export const dynamic = 'force-dynamic'
 
 export async function GET(request: Request) {
-  const response = await getTopTracks()
+  const { searchParams } = new URL(request.url)
+  const term = searchParams.get('term')
+
+  const response = await getTopTracks(term as TimeRange)
   const { items } = await response.json()
 
   const tracks = items.map((track: any) => ({
@@ -12,7 +15,9 @@ export async function GET(request: Request) {
     artist: track.artists.map(artist => artist.name).join(', '),
     songUrl: track.external_urls.spotify,
     title: track.name,
-    albumImageUrl: track.album.images[0].url
+    albumImageUrl: track.album.images[0].url,
+    term,
+    track_id: track.id
   }))
 
   return NextResponse.json(tracks, {
