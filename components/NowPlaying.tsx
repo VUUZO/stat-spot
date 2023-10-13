61c2bfa804eedd1093f421f7fa04404fc89aeb29
Oskar <kaplann.oskar@gmail.com>
2023-10-13 22:57:40 +0200

    Removed color feature

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -4,9 +4,6 @@ import fetcher from "@/lib/fetcher"
 import useSWR from "swr"
 import { PlayerTemplate } from "./PlayerTemplate"
 import NowPlayingSkeleton from "./skeleton/SkeletonNowPlaying"
-import { useColor } from "color-thief-react"
-import { useContext, useEffect } from "react"
-import ColorContext from "./ColorProvider"
 
 type NowPlaying = {
   title: string
@@ -21,12 +18,10 @@ const recent: NowPlaying = {
   songUrl: "https://open.spotify.com/track/0hL9gOw6XBWsygEUcVjxEc",
   albumImageUrl: "https://i.scdn.co/image/ab67616d0000b273881d8d8378cd01099babcd44",
   artist: "Travis Scott",
-  isPlaying: false
+  isPlaying: false,
 }
 
 const NowPlaying = () => {
-  const context = useContext(ColorContext)
-
   const {
     data: playing,
     isLoading,
@@ -35,13 +30,6 @@ const NowPlaying = () => {
 
   let currentImage = playing?.isPlaying ? playing.albumImageUrl : recent.albumImageUrl
 
-  const { data: color, loading, error: err } = useColor(currentImage, 'hex', { crossOrigin: 'anonymous' })
-  
-  useEffect(() => { 
-      context.setContextColor(color!)
-  }, [loading])
-  
-  
   return (
         isLoading ? (
           <NowPlayingSkeleton />
