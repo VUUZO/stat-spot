9c20125baed451470d373f546ea27c12ee40c9df
Oskar <kaplann.oskar@gmail.com>
2023-10-13 23:07:03 +0200

    Removed unused color context

--- components/GridLayout.tsx
+++ components/GridLayout.tsx
@@ -1,21 +1,15 @@
 'use client'
 
 import { ReactNode, useState } from "react"
-import ColorContext from "./ColorProvider"
 import { twMerge } from "tailwind-merge"
 
 export const GridLayout = ({ children }: { children: ReactNode }) => {
-  const [contextColor, setContextColor] = useState('#91FF5E')
 
   return (
-    <ColorContext.Provider value={{ contextColor, setContextColor }}>
-      <section
-      style={{ background: `linear-gradient(${contextColor}40 0%, transparent 32%), #131313` }}
-      className={twMerge(`text-white h-[100dvh] bg-dark gap-[10px] p-[10px] grid grid-areas-layout-mobile grid-cols-layout-mobile grid-rows-layout-mobile lg:grid-areas-layout lg:grid-cols-layout lg:grid-rows-layout`
+      <section className={twMerge(`text-white h-[100dvh] bg-dark gap-[10px] p-[10px] grid grid-areas-layout-mobile grid-cols-layout-mobile grid-rows-layout-mobile lg:grid-areas-layout lg:grid-cols-layout lg:grid-rows-layout`
       )}
       >
         { children }
       </section>
-    </ColorContext.Provider>
   )
 }
