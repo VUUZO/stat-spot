53640ae2f464100e864a07ee96e7d9cf5183dc34
Oskar <kaplann.oskar@gmail.com>
2023-10-13 22:58:33 +0200

    Fixed route

--- app/api/top-artists/route.ts
+++ app/api/top-artists/route.ts
@@ -1,10 +1,14 @@
+import { Term } from "@/app/context/term-context"
 import { getTopArtists } from "@/lib/spotify"
 import { NextResponse } from "next/server"
 
 export const dynamic = 'force-dynamic'
 
-export async function GET() {
-  const response = await getTopArtists()
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const term = searchParams.get('term')
+
+  const response = await getTopArtists(term as Term)
   const { items } = await response.json()
 
   const artists = items.map((artist: any) => ({
