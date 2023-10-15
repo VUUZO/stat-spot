7e644ea4ce7f780a09cb85dc7e05c7a7d5de42d6
Oskar <kaplann.oskar@gmail.com>
2023-10-15 14:23:33 +0200

    Changed rounded to 16px

--- components/Container.tsx
+++ components/Container.tsx
@@ -9,7 +9,7 @@ export const containerVariants = cva(
   {
     variants: {
       variant: {
-        default: 'relative bg-gray-dark rounded-lg after:rounded-[inherit] after:pointer-events-none after:inset-0 after:absolute after:shadow-[inset_0_1px_0_rgb(255,255,255,.05),inset_0_-1px_0_rgb(255,255,255,.01),inset_1px_0_0_rgb(255,255,255,.02),inset_-1px_0_0_rgb(255,255,255,.02)]',
+        default: 'relative bg-gray-dark rounded-[16px] after:rounded-[inherit] after:pointer-events-none after:inset-0 after:absolute after:shadow-[inset_0_1px_0_rgb(255,255,255,.05),inset_0_-1px_0_rgb(255,255,255,.01),inset_1px_0_0_rgb(255,255,255,.02),inset_-1px_0_0_rgb(255,255,255,.02)]',
         transparent: '',
       },
       opacity: {
