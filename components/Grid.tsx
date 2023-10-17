d69b4caf6de3f244a7995449bbe073faf08bcc3b
Oskar <kaplann.oskar@gmail.com>
2023-10-17 23:02:48 +0200

    Changed grid sizes

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,9 +1,8 @@
 import { ReactNode } from "react"
-import { Container } from "./Container"
 
 export const Grid = ({ children }: { children: ReactNode }) => {
   return (
-    <ul className="grid gap-3 grid-cols-3 lg:grid-cols-[repeat(auto-fit,minmax(350px,1fr))]">
+    <ul className="grid gap-[10px] grid-cols-3 lg:grid-cols-[repeat(auto-fit,minmax(220px,1fr))]">
       { children }
     </ul>
   )
