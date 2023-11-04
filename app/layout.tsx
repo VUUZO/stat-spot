a647cb7c2a52b38bf3c0a4364620bdfd9b4448c7
720 <kaplann.oskar@gmail.com>
2023-11-04 16:10:43 +0100

    Updated layout images

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,21 +54,24 @@ export default function RootLayout({
     >
       <body>
         
-        <img className='-z-10 pointer-events-none select-none w-full fixed top-0 left-0' src="/images/blob001.png" alt="blob" />
-        <img className='-z-10 pointer-events-none select-none w-full fixed bottom-0 right-0' src="/images/blob002.png" alt="blob" />
+          <img className='-z-10 pointer-events-none select-none w-full lg:w-1/3 fixed top-0 left-0' src="/images/blob001.png" alt="blob" />
+          <img className='-z-10 pointer-events-none select-none w-full lg:w-1/2 fixed bottom-0 right-0' src="/images/blob002.png" alt="blob" />
         
-        <section className='max-w-7xl mx-auto'>
-          <TermProvider>
-            <div className='sticky top-0 z-50'>
-              <div className='absolute -z-10 inset-0 saturate-150 [mask-image:linear-gradient(black_20%,rgb(0,0,0,.8)_80%,transparent_90%)] bg-gray-darker'/>
+        <TermProvider>
+          <div className='sticky top-0 z-50'>
+            <div className='absolute -z-10 inset-0 saturate-150 [mask-image:linear-gradient(black_20%,rgb(0,0,0,.8)_80%,transparent_90%)] bg-gray-darker'/>
+            <div className='max-w-7xl mx-auto'>
               <Navigation />
               <TermNavigation />
             </div>
-            <Container variant={'transparent'} className='flex flex-col gap-[10px]'>
-              {children}
-            </Container>
-          </TermProvider>
-        </section>
+          </div>
+
+          <section className='max-w-7xl mx-auto'>
+              <Container variant={'transparent'} className='flex flex-col gap-[10px]'>
+                {children}
+              </Container>
+          </section>
+        </TermProvider>
       </body>
     </html>
   )
